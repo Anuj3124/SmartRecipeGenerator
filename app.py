468bc9b306c206e37508b701335b654efbfd6852
Anuj3124 <anujsenthil3124@gmail.com>
2025-05-17 14:59:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -186,5 +186,7 @@ def nlp_search():
 
     return jsonify({"recipes": results, "included": included, "excluded": excluded})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port
+    app.run(host="0.0.0.0", port=port)
